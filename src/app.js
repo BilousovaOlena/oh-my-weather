99a7286fda8cc4edac802e9d8e57633de773011f
BilousovaOlena <bellollena@gmail.com>
2022-07-13 16:11:40 +0200

    Create date time JS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
+function updateTime(timestamp) {
+  let date = new Date(timestamp);
+  let minuts = date.getMinutes();
+  if (minuts < 10) {
+    minuts = `0${minuts}`;
+  }
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let dayWeek = days[date.getDay()];
+  return `${dayWeek} ${hours}:${minuts}`;
+}
+
 function showParameters(response) {
-  console.log(response.data);
   let nameCity = document.querySelector("#city-name");
   nameCity.innerHTML = response.data.name;
   let countryName = document.querySelector("#country-name");
@@ -12,6 +34,8 @@ function showParameters(response) {
   humidityElem.innerHTML = response.data.main.humidity;
   let windElem = document.querySelector("#wind");
   windElem.innerHTML = Math.round(response.data.wind.speed);
+  let timeElement = document.querySelector("#day-time");
+  timeElement.innerHTML = updateTime(response.data.dt * 1000);
 }
 
 let apiKey = "eaf040dff7892fc80bc32a2d99e6ebec";
